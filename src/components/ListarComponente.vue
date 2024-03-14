a27acb6d59cc987399dc89cdf53bc982bbce91f3
Neydi <LE17081469@merida.tecnm.mx>
2024-03-14 10:04:22 -0600

    first commit

---
+++ src/components/ListarComponente.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="container">
+        <router-link to="/CrearComponente" class="btn btn-success"> Agregar nuevo empleado</router-link>
+        <br/>
+        <br/>
+
+       <div>
+        <div class="card">
+            <div class="card-header">
+                Empleados
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Correo</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="empleado in empleados" :key="empleado.id">
+                            <td>{{empleado.id}}</td>
+                            <td>{{empleado.nombre}}</td>
+                            <td>{{empleado.correo}}</td>
+                            <td>    
+                                <div class="btn-group" role="group" aria-label="">
+                                    <router-link :to="{name:'EditarComponente',params:{id:empleado.id}}" class="btn btn-info">Editar</router-link>
+                                    <button type="button" class="btn btn-danger" v-on:click="borrarEmpleado(empleado.id)">Borrar</button>
+                                </div>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+                
+            </div>
+            
+        </div>
+       </div>
+        <div >
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            empleados:[]
+        }
+    },
+    created:function(){
+        this.consultarEmpleados();
+
+    },
+    methods:{
+        consultarEmpleados(){
+            fetch('http://localhost/empleados/')
+            .then(respuesta=>respuesta.json())
+            .then((datosRespuesta)=>{
+                console.log(datosRespuesta)
+                this.empleados=[]
+                if(typeof datosRespuesta[0].success==='undefined' )
+                {
+                    this.empleados=datosRespuesta;
+                }
+            })
+            .catch(console.log)
+        },
+        borrarEmpleado(id){
+            console.log(id);
+            fetch('http://localhost/empleados/?borrar='+id)
+            .then(respuesta=>respuesta.json())
+            .then((datosRespuesta)=>{
+                console.log(datosRespuesta)
+                window.location.href="ListarComponente" 
+            })
+            .catch(console.log)
+        }
+    }
+};
+</script>
